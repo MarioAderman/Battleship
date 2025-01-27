155184ed4b824b710c01f0702224bc799b45e673
Mario Aderman <MarioAderman@DESKTOP-LMC6SL1.>
2025-01-27 10:33:49 -0600

    Fixed placing ship function

--- main.py
+++ main.py
@@ -32,40 +32,36 @@ def placing_ship(gameboard, ship):
         if ship.lr_ud_orient.upper() == "L":
             for i in range (0, ship.size):
                 if ship.size == 2:
-                    print(ship.row_coordinate)
-                    print(ship.col_coordinate)
-                    print(ship.row_coordinate - 10)
-                    print(ship.col_coordinate - (i - 1))
-                    gameboard[ship.row_coordinate - 10][ship.col_coordinate - i] = "B"
+                    gameboard[10 - ship.row_coordinate][ship.col_coordinate - (i - 1)] = "B"
                 elif ship.size == 3:
-                    gameboard[ship.row_coordinate - 10][ship.col_coordinate - (i - 1)] = "S"
+                    gameboard[10 - ship.row_coordinate][ship.col_coordinate - (i - 1)] = "S"
                 elif ship.size == 4:
-                    gameboard[ship.row_coordinate - 10][ship.col_coordinate - (i - 1)] = "D"
+                    gameboard[10 - ship.row_coordinate][ship.col_coordinate - (i - 1)] = "D"
         elif ship.lr_ud_orient.upper() == "R":
             for i in range (0, ship.size):
                 if ship.size == 2:
-                    gameboard[ship.row_coordinate - 10][ship.col_coordinate + (i - 1)] = "B"
+                    gameboard[10 - ship.row_coordinate][ship.col_coordinate + (i - 1)] = "B"
                 elif ship.size == 3:
-                    gameboard[ship.row_coordinate - 10][ship.col_coordinate + (i - 1)] = "S"
+                    gameboard[10 - ship.row_coordinate][ship.col_coordinate + (i - 1)] = "S"
                 elif ship.size == 4:
-                    gameboard[ship.row_coordinate - 10][ship.col_coordinate + (i - 1)] = "D"
+                    gameboard[10 - ship.row_coordinate][ship.col_coordinate + (i - 1)] = "D"
     else:
         if ship.lr_ud_orient.upper() == "U":
             for i in range (0, ship.size):
                 if ship.size == 2:
-                    gameboard[ship.row_coordinate - i][ship.col_coordinate - 1] = "B"
+                    gameboard[10 - ship.row_coordinate - i][ship.col_coordinate - 1] = "B"
                 elif ship.size == 3:
-                    gameboard[ship.row_coordinate - i][ship.col_coordinate - 1] = "S"
+                    gameboard[10 - ship.row_coordinate - i][ship.col_coordinate - 1] = "S"
                 elif ship.size == 4:
-                    gameboard[ship.row_coordinate - i][ship.col_coordinate - 1] = "D"
+                    gameboard[10 - ship.row_coordinate - i][ship.col_coordinate - 1] = "D"
         elif ship.lr_ud_orient.upper() == "D":
             for i in range (0, ship.size):
                 if ship.size == 2:
-                    gameboard[ship.row_coordinate + i][ship.col_coordinate - 1] = "B"
+                    gameboard[10 - ship.row_coordinate + i][ship.col_coordinate - 1] = "B"
                 elif ship.size == 3:
-                    gameboard[ship.row_coordinate + i][ship.col_coordinate - 1] = "S"
+                    gameboard[10 - ship.row_coordinate + i][ship.col_coordinate - 1] = "S"
                 elif ship.size == 4:
-                    gameboard[ship.row_coordinate + i][ship.col_coordinate - 1] = "D"
+                    gameboard[10 - ship.row_coordinate + i][ship.col_coordinate - 1] = "D"
     return gameboard
 
 def val_placing(ship):
@@ -99,21 +95,20 @@ def val_placing(ship):
 for i in range (0,3):
     ships = ["Boat", "Submarine", "Destroyer"]
     #Boat addition
-    col = int(input(f"Enter X or column coordinate to locate your {ships[i]}: "))
-    row = int(input(f"Enter Y or row coordinate to locate your {ships[i]}: "))
-    orient_hv = input(f"Enter Horizontal or Vertical orientation of your {ships[i]}: ")
+    col = int(input(f"Enter column or X coordinate to locate your {ships[i]}: "))
+    row = int(input(f"Enter row or Y coordinate to locate your {ships[i]}: "))
+    orient_hv = input(f"Enter Horizontal (H) or Vertical (V) orientation of your {ships[i]}: ")
     if orient_hv.upper() == "H":
         orient_hv = True
-        orient_lr_ud = input(f"Enter Left or Right orientation of your {ships[i]}: ")
+        orient_lr_ud = input(f"Enter Left (L) or Right (R) orientation of your {ships[i]}: ")
     elif orient_hv.upper() == "V":
         orient_hv = False
-        orient_lr_ud = input(f"Enter Up or Down orientation of your {ships[i]}: ")
+        orient_lr_ud = input(f"Enter Up (U) or Down (D) orientation of your {ships[i]}: ")
     else:
         print("Invalid orientation. Please enter H for Horizontal or V for Vertical.")
         
     ship = Shipset(2 + i, col, row, orient_hv, orient_lr_ud)
     is_place_check = val_placing(ship)
-    print(is_place_check)
     if is_place_check is True:
         gameboard = placing_ship(gameboard, ship)
         print("Boat placed successfully.")
